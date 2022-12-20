3fc6058e4c96a2a95a94b6b4202965c4fb60b847
seismolog-bit <si.sintetis@gmail.com>
2022-12-20 16:10:57 +0700

    time table

--- resources/views/public/fetch-order.blade.php
+++ resources/views/public/fetch-order.blade.php
@@ -1,13 +1,15 @@
 <div class="row justify-content-center m-0">
 
 @foreach ($datas as $order)
-<div class="col-md-4 col-lg-3 py-3">
+<div class="col-md-4 col-lg-3 py-4">
     <div class="card card-bordered">
-        <div class="card-body p-3">
+        <div class="card-body p-4">
             <div class="row">
-                <div class="col-sm-6">{{ $order->table->name }} - Lantai {{ $order->table->floor }}</div>
+                <div class="col-sm-6">
+                    <h1>{{ $order->table->name }} - Lantai {{ $order->table->floor }}</h1>
+                </div>
                 <div class="col-sm-6 text-end">
-                    <h4 data-countdown="{{ Carbon\Carbon::parse($order->end_time)}}"></h4>
+                    <h1 data-countdown="{{ Carbon\Carbon::parse($order->end_time)}}"></h1>
                 </div>
             </div>
         </div>
